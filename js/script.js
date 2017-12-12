a78c218d11ac2e8e620cf50166be16235adba00b
Karts <9369992@gmail.com>
2017-12-12 17:01:26 +0300

    second commint

--- js/script.js
+++ js/script.js
@@ -1,143 +1,159 @@
+const statusCancel = 'Cancel';
+const statusDone = 'Done';
+const remove = 'Remove';
+const classSuccess = 'trSuccess';
+
 window.onload = function() {
     document.getElementById('file-input').addEventListener('change', readSingleFile, false); // listener to open file
     currentDate();
-    var dataString = localStorage.getItem("list_tr");
-    var data = JSON.parse(dataString);
-    for (var i = data.length - 1; i >= 0; i--) {
-        addFunction(data[i].text, data[i].date, data[i].success);
+    let dataString = localStorage.getItem('list_tr');
+    let data = JSON.parse(dataString);
+    for (let i = data.length - 1; i >= 0; i--) {
+        addItem(data[i].text, data[i].date, data[i].success);
     }
 }
 
 
-function clickFunction() {
-
-    savefile(); //----test
-
-    if (document.getElementById("todo").value !== '') {
-        addFunction(document.getElementById("todo").value, document.getElementById("meeting").value, false);
-    } else {
-        alert("TODO input field is empty!");
+function clickToCreate() {
+    if (document.getElementById('todo').value) {
+        addItem(document.getElementById('todo').value, document.getElementById('meeting').value, false);
+        return;
     }
+
+    alert('TODO input field is empty!');
 }
 
-function addFunction(text, date, success) {
-    var table = document.getElementById("table");
-    var tdText = document.createElement("td");
-    var tdDate = document.createElement("td");
-    var tr = document.createElement("tr");
+function addItem(text, date, success) {
+    let flexWrap = document.getElementsByClassName('flex-wrap')[0];
+    let divText = document.createElement('div');
+    let divDate = document.createElement('div');
+    let divRow = document.createElement('div');
 
-    tdText.innerHTML = text;
-    tdDate.innerHTML = date;
-    tr.appendChild(tdText);
-    tr.appendChild(tdDate);
+    console.log(flexWrap);
+
+    divText.innerHTML = text;
+    divDate.innerHTML = date;
+    divRow.appendChild(divText);
+    divRow.appendChild(divDate);
     if (success) {
-        tr.className = "trSuccess";
+        divRow.className = classSuccess + ' ';
     }
-    addButton(tr);
-    table.appendChild(tr);
+    addButton(divRow);
+    divRow.className += 'flex-rows';
+    flexWrap.appendChild(divRow);
     saveData();
 
 }
 
 function saveData() {
+    let tableArr = document.getElementsByClassName('flex-wrap')[0].childNodes;
+    let arrayData = [];
 
-    var tableArr = document.getElementById("table").childNodes;
-    var ArrayData = [];
-
-    for (var i = tableArr.length - 1; i > 0; i--) {
-        ArrayData.push({
+    for (let i = tableArr.length - 1; i > 0; i--) {
+        arrayData.push({
             'text': tableArr[i].childNodes[0].innerHTML,
             'date': tableArr[i].childNodes[1].innerHTML,
-            'success': tableArr[i].classList.contains("trSuccess")
+            'success': tableArr[i].classList.contains(classSuccess)
         })
     }
-    localStorage.setItem("list_tr", JSON.stringify(ArrayData));
+    localStorage.setItem('list_tr', JSON.stringify(arrayData));
 
 }
 
-function addButton(tr) {
-    var tdSuccess = document.createElement("td");
-    var tdRemove = document.createElement("td");
+function addButton(divRow) {
+    let divSuccess = document.createElement('div');
+    let divRemove = document.createElement('div');
 
-    tdSuccess.className = "SuccessButton";
-    tdRemove.className = "RemoveButton";
+    divSuccess.className = 'successButton';
+    divRemove.className = 'removeButton';
 
-    if (tr.classList.contains("trSuccess")) {
-        tdSuccess.innerHTML = "Cansel";
+    if (divRow.classList.contains(classSuccess)) {
+        divSuccess.innerHTML = statusCancel;
     } else {
-        tdSuccess.innerHTML = "Done";
+        divSuccess.innerHTML = statusDone;
     }
-    tdRemove.innerHTML = "Remove";
+    divRemove.innerHTML = remove;
 
-    tdSuccess.onclick = function changeStatus(event) {
-        var tr = event.target.parentElement;
+    divSuccess.onclick = function changeStatus(event) {
+        let divRow = event.target.parentElement;
         switch (this.innerHTML) {
-            case "Done":
+            case statusDone:
                 {
-                    tr.className = "trSuccess";
-                    this.innerHTML = "Cansel";
+                    divRow.classList.add(classSuccess);
+                    this.innerHTML = statusCancel;
                     break;
                 };
-            case "Cansel":
+            case statusCancel:
                 {
-                    tr.className = "";
-                    this.innerHTML = "Done";
+                    divRow.classList.remove(classSuccess);
+                    this.innerHTML = statusDone;
                     break;
                 }
         }
         saveData();
     }
 
-    tdRemove.onclick = function changeStatus(event) {
-        var tr = event.target.parentElement;
-        tr.remove();
+    divRemove.onclick = function changeStatus(event) {
+        let removeDiv = event.target.parentElement;
+        removeDiv.remove();
         saveData();
     }
 
-    tr.appendChild(tdSuccess);
-    tr.appendChild(tdRemove);
+    divRow.appendChild(divSuccess);
+    divRow.appendChild(divRemove);
 }
 
 function currentDate() {
-    document.getElementById("meeting").valueAsDate = new Date();
+    document.getElementById('meeting').valueAsDate = new Date();
 }
 
 function readSingleFile(e) {
-    var file = e.target.files[0];
+    let file = e.target.files[0];
     if (!file) {
         return;
     }
-    var reader = new FileReader();
+    let reader = new FileReader();
 
     reader.onload = function(e) {
-        var contents = e.target.result;
+        let contents = e.target.result;
         displayContents(contents);
     };
     reader.readAsText(file);
+    this.value = '';
 }
 
 function displayContents(contents) {
-    var data = JSON.parse(contents);
-    for (var i = data.length - 1; i >= 0; i--) {
-        addFunction(data[i].text, data[i].date, data[i].success);
+	console.log("+");
+    let data = JSON.parse(contents);
+    for (let i = data.length - 1; i >= 0; i--) {
+        addItem(data[i].text, data[i].date, data[i].success);
     }
 
 }
 
-function savefile() {
+function saveFile() {
+    let tableArr = document.getElementsByClassName('flex-wrap')[0];
+    	tableArr = tableArr.children;
+    let arrayData = [];
+    for (let i = tableArr.length - 1; i > 0; i--) {
 
-    var tableArr = document.getElementById("table").childNodes;
-    var ArrayData = [];
-
-    for (var i = tableArr.length - 1; i > 0; i--) {
-        ArrayData.push({
+        arrayData.push({
             'text': tableArr[i].childNodes[0].innerHTML,
             'date': tableArr[i].childNodes[1].innerHTML,
-            'success': tableArr[i].classList.contains("trSuccess")
+            'success': tableArr[i].classList.contains(classSuccess)
         })
     }
 
-    uriContent = "data:application/octet-stream," + encodeURIComponent(JSON.stringify(ArrayData));
+    uriContent = 'data:application/octet-stream,' + encodeURIComponent(JSON.stringify(arrayData));
 
-    document.getElementById("test").innerHTML = "<a href=" + uriContent + " download=\"savedfile.txt\">Here is the download link</a>";
+    document.getElementById('save').href = uriContent;
+    document.getElementById('save').download = 'data.txt';
+}
+
+function clearTable() {
+	let parent = document.getElementsByClassName('flex-wrap')[0];
+    for (var i = parent.children.length - 1; i >= 0; i--) {
+        parent.children[i].remove();
+    }
+    saveData();
 }
